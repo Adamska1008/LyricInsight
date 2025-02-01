b83207100395da08fa139f697dcf347384989564
Zijian Zang <2639980868@qq.com>
2025-02-01 21:30:02 +0800

    feat: update SplitJob class

--- main.py
+++ main.py
@@ -8,8 +8,7 @@ import prompts
 
 class SplitJob:
     """
-    歌词划分的参数。
-    当通过多次请求来翻译一首歌时，每次请求应当翻译几句歌词。
+    Describe how lyrics will be divided
     """
 
     single_lyrics_lines: int  # 一句歌词占几行
@@ -17,7 +16,10 @@ class SplitJob:
     remove_blank_lines: bool
 
     def __init__(
-        self, sinlgle_lyrics_lines: int, lyrics_per_group: int, remove_blank_lines=True
+        self,
+        sinlgle_lyrics_lines: int,
+        lyrics_per_group: int = None,
+        remove_blank_lines=True,
     ):
         self.single_lyrics_lines = sinlgle_lyrics_lines
         self.lyrics_per_group = lyrics_per_group
@@ -26,29 +28,35 @@ class SplitJob:
     @typeguard.typechecked
     def apply(self, inp: str) -> list[str]:
         """
-        Apply the split job on the input str
+        Apply the split job on the input str.
+        If self.lyrics_per_group is None, return a list with single element (the whole lyrics)
         """
         inps = inp.splitlines()
+        # divide `self.single_lyrics_lines` lines into a group
         inps = [
             inps[i : i + self.single_lyrics_lines]
             for i in range(0, len(inps), self.single_lyrics_lines)
         ]
+        # remove blank lines
         for i, group in enumerate(inps):
             inps[i] = [inp for inp in group if len(inp) != 0]
         inps = ["\n".join(inp) for inp in inps]
-        inps = [
-            "\n".join(inps[i : i + self.lyrics_per_group])
-            for i in range(0, len(inps), self.lyrics_per_group)
-        ]
+        if self.lyrics_per_group:
+            inps = [
+                "\n".join(inps[i : i + self.lyrics_per_group])
+                for i in range(0, len(inps), self.lyrics_per_group)
+            ]
+        else:
+            inps = ["\n".join(inps)]
         return inps
 
 
 @typeguard.typechecked
 def transform(inp: str, job: SplitJob) -> str:
-    client = OpenAI(
-        base_url=os.getenv("DEEPSEEK_BASE_URL"), api_key=os.getenv("DEEPSEEK_API_KEY")
-    )
-
+    # client = OpenAI(
+    #     base_url=os.getenv("DEEPSEEK_BASE_URL"), api_key=os.getenv("DEEPSEEK_API_KEY")
+    # )
+    client = OpenAI()
     # split input
     inps = job.apply(inp)
 
@@ -58,7 +66,7 @@ def transform(inp: str, job: SplitJob) -> str:
         logger.info(f"处理第{index + 1}组歌词")
         logger.info(f"输入:\n{inp}")
         completion = client.chat.completions.create(
-            model="deepseek-chat",
+            model="gpt-4o-mini",
             messages=[
                 {"role": "system", "content": prompts.sys_prompt},
                 {"role": "user", "content": prompts.examples[0][0]},
@@ -80,7 +88,7 @@ def main():
     with open(filename, "r", encoding="UTF-8") as f:
         content = f.read()
     print("歌词转换中……")
-    transformed = transform(content, SplitJob(5, 3))
+    transformed = transform(content, SplitJob(5, 4))
     base_name, extension = os.path.splitext(filename)
     new_filename = f"{base_name}_transformed.{extension}"
     with open(new_filename, "a", encoding="UTF-8") as f:
--- tests/test_main.py
+++ tests/test_main.py
@@ -12,7 +12,7 @@ sys.path.append(os.path.abspath(os.path.dirname(__file__) + "/.."))
 
 
 def test_split():
-    inp = """花道を薄く照らして（淡薄照亮花道）
+    lyrics = """花道を薄く照らして（淡薄照亮花道）
 
 hanamichi wo usuku terashite
 
@@ -62,28 +62,35 @@ kawaranai koe de douka sasayaite
 kowareta kokoro wo semete tsutsunde
 """
     expected = [
-        """花道を薄く照らして（淡薄照亮花道）
+        [
+            """花道を薄く照らして（淡薄照亮花道）
 hanamichi wo usuku terashite
 寄せ木細工　音を奏でた（拼貼木紋　奏出音色）
 yosegizaiku oto wo kanadeta
 艶やかな　上弦の月（艷麗的　上弦之月）
 adeyaka na jougen no tsuki""",
-        """雲に消えた　傘もないのに（消失於雲中　明是手中也無傘）
+            """雲に消えた　傘もないのに（消失於雲中　明是手中也無傘）
 kumo ni kieta kasa mo nai noni
 朝がきてそれが春の霜解（しもど）けの様に（黎明到來那就如春日雪融般）
 asa ga kite sore ga haru no shimodoke no youni
 凍てついた戀がいつか熱く流れるならば（冰凍的愛戀若將有日　熾熱流淌）
 itetsuita koi ga itsuka atsuku nagareru naraba""",
-        """終わらない雨の中で抱きしめて（就在不止的雨中緊緊擁抱吧）
+            """終わらない雨の中で抱きしめて（就在不止的雨中緊緊擁抱吧）
 owaranai ame no naka de dakishimete
 貴方が答えを隠しているのなら（若你藏起了解答）
 anata ga kotae wo kakushite iru no nara
 変わらない聲でどうか囁いて（至少請用不變的聲音呢喃）
 kawaranai koe de douka sasayaite""",
-        """壊れた心をせめて包んで（包容那毀壞的心吧）
+            """壊れた心をせめて包んで（包容那毀壞的心吧）
 kowareta kokoro wo semete tsutsunde""",
+        ],
     ]
     job = SplitJob(5, 3)
-    inps = job.apply(inp)
+    inps = job.apply(lyrics)
     for i, inp in enumerate(inps):
-        assert expected[i] == inp
+        assert expected[0][i] == inp
+    job = SplitJob(5, 5)
+    inps = job.apply(lyrics)
+    for i, inp in enumerate(inps):
+        print(i)
+        print(inp)
